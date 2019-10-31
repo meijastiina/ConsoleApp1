e615750f5b5266dcd3b6c74d76ca060bb22dba03
Meija Lohiniva <meija.lohiniva@seamk.fi>
2019-10-31 18:20:35 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            do
+            {
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ohjelman suoritus menee tällä rivillä.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occured ( { ex.Message } ).");
+                }
+                finally
+                {
+                    Console.WriteLine("Tämä tulostetaan aina.");
+                }
+            } while ();
+        }
+    }
+}
